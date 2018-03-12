cb7f04d59d3de5e661aee468960257f9fa739e02
David Harlow <davidaharlow@gmail.com>
2018-03-12 14:59:05 -0700

    added basic database testing

--- app/server/api/exchangeRates.js
+++ app/server/api/exchangeRates.js
@@ -3,14 +3,22 @@ const axios = require('axios');
 const db = require('../../database/index.js');
 const { currencies } = require('../helpers/constants.js');
 
+// To Do: Create tables for storing historical exchange rate info  
+// To Do: Create Cron Job to get the exchange rates for each currency and store to database
 const updateExchangeRates = async (currency) => {
 	const exchangeRatesByCurrency = await axios(`http://api.fixer.io/latest?base=${currency}`);
-	// db.insert
+	// db.insert function
 };
 
-// const recurringExchangeRate = cron.schedule('5 * * * * *', () => {
-//   updateExchangeRates();
-// }, false);
+// To Do: Once ready, set this to happen once per day at 8am
+const recurringExchangeRate = cron.schedule('5 * * * * *', () => {
+	const currencyCodes = currencies.map(currency => currency.code);
 
+	currencyCodes.forEach((currency) => {
+  	updateExchangeRates(currency);
+  });
+  
+}, false);
+
+// uncomment to begin cron job
 // recurringStatUpdate.start();
-
--- test/database.js
+++ test/database.js
@@ -1,12 +1,11 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { expect } = require('chai');
-const server = require('../app/server/app.js');
+const { server } = require('../app/server/app.js');
 const { client } = require('../app/database');
-const { createOrder } = require('../app/database/helpers.conversionOrders.js');
+const { createOrder } = require('../app/database/helpers/conversionOrders.js');
 require('dotenv').config();
 chai.use(chaiHttp);
-
 
 describe('Database', () => {
 	before(() => {
@@ -16,24 +15,25 @@ describe('Database', () => {
     server.close(() => console.log(`server no longer listening on port ${process.env.PORT}`))
   });
 
-	describe('Table: Events', () => {
-	  it('should return event object for valid timeframe name in database', async (done) => {
-	    const query = createOrder('playlistServedEvent', message);
-	    client.execute(query.query, query.params)
-	      .then(result => console.log('User with email %s', result.rows[0].email));
+		describe('Table: Events', () => {
+		  it('should return event object for valid timeframe name in database', (done) => {
+		  	client.query('SELECT id FROM concurrency_converter.users WHERE username = $1', ['David Harlow'])
+		  		.then(userId => {
+		  			console.log(userId)
+				  	expect(userId.rows[0].id).to.be.a('number');
+				  	expect(userId.rows[0].id).to.equal(1);
+				  	done();
+		  		});
+		  }).timeout(1000);
 
-	    setTimeout(() => {
-	      const queryResult = {};
-	      queryResult.id = 10000000;
-	      expect(queryResult.id).to.be(10000000);
-	    }, 500);
-	  }).timeout(1000);
-
-	  it('should return nothing for invalid timeframe name not in database', (done) => {
-
-	  }).timeout(1000);
-  
+		  it('should return nothing for invalid timeframe name not in database', (done) => {
+		  	client.query('SELECT id FROM concurrency_converter.users WHERE username = $1', ['Unreal NotName'])
+		  		.then(userId => {
+				  	console.log(userId)
+				  	expect(userId).not.to.be.a('number');
+				  	done();
+		  		});
+		  }).timeout(1000);
+	  
+	});
 });
-
-// id | username
-// id | user_id | orign_currency | destination_currency | exchange_rate | origin_amount | destination_amount | date
