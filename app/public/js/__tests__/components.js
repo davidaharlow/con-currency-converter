09a6e2dd26553377235a7a96dbc7ee494eb0d3e1
David Harlow <davidaharlow@gmail.com>
2018-03-23 14:46:38 -0700

    code cleanup

--- app/public/js/__tests__/components.js
+++ app/public/js/__tests__/components.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { ActionTypes as types } from '../helpers/constants';
 import Adapter from 'enzyme-adapter-react-16';
 import FeesTable from '../components/FeesTable';
 import Conversion from '../components/Conversion';
 
 Enzyme.configure({ adapter: new Adapter() });
  
-function setup() {
+function setupFeesTable() {
   const props = {
     conversionRate: 0,
     fee: 0,
@@ -23,10 +26,54 @@ function setup() {
   }
 }
 
+
+function setupConversion() {
+  const defaultState = {
+    originAmount: 10,
+    destinationAmount: 10,
+    originCurrency: 'USD',
+    destinationCurrency: 'USD',
+    conversionRate: 1,
+    feeAmount: 0,
+    totalCost: 0,
+    username: 'David Harlow',
+    mostRecentUsername: 'David Harlow',
+    date: 11052018,
+    mostRecentOriginAmount: 10,
+    mostRecentOriginCurrency: 'USD',
+    mostRecentDestinationAmount: 10,
+    mostRecentDestinationCurrency: 'USD',
+    errorMsg: null,
+  }
+
+  const props = defaultState
+
+  const order Reducer = (state = defaultState, action) => {
+    switch (action.type) {
+      case (types.CHANGE_USERNAME):
+        return {
+          ...state,
+          username: action.data.username
+       }
+      default:
+        return state;
+    }
+  }
+
+  const store = createStore(orderReducer);
+ 
+  const enzymeWrapper = mount(<Provider store={store}><Conversion {...props}/></Provider>)
+ 
+  return {
+    props,
+    enzymeWrapper
+  }
+}
+
 describe('components', () => {
   describe('FeesTable', () => {
     it('should render self and subcomponents', () => {
-      const { enzymeWrapper } = setup()
+      const { enzymeWrapper } = setupFeesTable()
  
       expect(enzymeWrapper.find('table').hasClass('mui-table')).toBe(true)
       expect(enzymeWrapper.find('.td-title').text()).toBe('Conversion Rate')
@@ -44,6 +91,18 @@ describe('components', () => {
     //   expect(props.addTodo.mock.calls.length).toBe(1)
     // })
   })
+
+  describe('Conversion', () => {
+    it('should render self and subcomponents', () => {
+      const { enzymeWrapper } = setupConversion()
+ 
+      // expect(enzymeWrapper.find('titlePT1').hasClass('mui-table')).toBe(true)
+      expect(enzymeWrapper.find('.titlePT1').text()).toBe('Bank of Harlow')
+      // expect(enzymeWrapper.find('.total-label').text()).toBe('Total Cost')
+      // expect(enzymeWrapper.find('.fee').text()).toBe('0.00 USD')
+       
+    })
+  })
 })
 
 
@@ -58,3 +117,4 @@ describe('A suite', () => {
   });
 
 });
+
